bf7b07268b3f69d8e9e193d916e8775e1ad77db1
Pauli Price <pauli.price@gmail.com>
2014-02-24 09:10:43 -0500

    fixed create test, describe beforeEach Datastore still broken

--- test/test-mocha.js
+++ test/test-mocha.js
@@ -5,33 +5,28 @@ var chai = require('chai'),
   PLH = require("../lib/polyhedron.js"),
   PouchDB = require('pouchdb'),
   should = chai.should(),
-  helper = require('./helper.js'),
-  tracer = require('../lib/tools/tracer.js');
+  helper = require('./helper.js')
+  ;
 
 chai.use(chaiAsPromised);  
-tracer.traceAll(PLH, true);
-
-//var DropboxAPI; // = require(../lib/servers/PouchAPI.js);
 
 describe("Library Interface:", function () {
 
   describe("create a database", function () {
     
     it("PouchDB", function (done) {
-      var ds = new PLH.datastore({database: 'testdb2', server: 'pouchdb'}).then(
-        function (reason) {
-          ds.should.eventually.be.an.instanceof(PLH.Datastore).notify(done);
-        });
+      var ds = PLH.datastore({database: 'testdb2', server: 'pouchdb'});
+      //ds.should.be.fulfilled.and.notify(done);
+      ds.should.eventually.be.an.instanceof(PLH.Datastore).notify(done);
     });
   
     it.skip("Dropbox", function (done) {
-      var ds = new PLH.datastore({database: 'testdb2', server: 'dropbox'});
+      var ds = PLH.datastore({database: 'testdb2', server: 'dropbox'});
       ds.should.eventually.be.an.instanceof(PLH.Datastore).notify(done);
     });
   });
   
   describe("delete a database", function () {
-    this.timeout(10000);
     
     it("PouchDB", function (done) {
       var ds = PLH.datastore({database: 'testdb2', server: 'pouchdb'})
@@ -56,5 +51,32 @@ describe("Library Interface:", function () {
     });
   });
   
-  tracer.untraceAll();
+});
+
+describe("Datastore", function () {
+  var ds;
+  
+  beforeEach(function (done) {
+    ds = PLH.datastore({database: 'testdb2', server: 'pouchdb'})
+         .then(function (data) {
+           console.log(data);
+            return data;
+         })
+         .then(function (data) {
+            done();
+         })
+         .catch(function (reason) {
+           done(reason);
+         });  
+  });
+
+  afterEach(function (done) {
+    // testUtils.cleanupTestDatabases();
+    done();
+  }); 
+  
+  it("should be an instance of PLH.Datastore", function (done) {
+    ds.should.eventually.be.an.instanceof(PLH.Datastore).notify(done);
+  });
+
 });
