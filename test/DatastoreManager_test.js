207e8afc276e718f7e0256076030f11d7d57ac26
Pauli Price <pauli.price@gmail.com>
2014-03-05 09:04:18 -0500

    start new design

---
+++ test/DatastoreManager_test.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var chai = require('chai'),
+  chaiAsPromised = require('chai-as-promised'),
+  PLH = require('../lib/polyhedron.js'),
+  PouchDB = require('pouchdb'),
+  should = chai.should(),
+  helper = require('./helper.js'),
+  circuitbox = require('circuitbox')
+  ;
+
+chai.use(chaiAsPromised);
+
+describe("DatastoreManager:", function () {
+
+  describe("create a DatastoreManager", function () {
+    
+    it("PouchDB", function (done) {
+    });
+  
+  });
+    
+});
